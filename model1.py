936828fe173b128828837a327709ba3e35cb1614
zgeorgin <zgeorginn@gmail.com>
2024-09-19 11:51:36 +0300

    Первая версия бота

---
+++ model1.py
@@ -0,0 +1,19 @@
+from langchain.schema import HumanMessage, SystemMessage
+from langchain.chat_models.gigachat import GigaChat
+
+chat = GigaChat(credentials = 'YTg1ZWYzZmItNDJmYi00ZmNmLWJlMzItN2ZhMTJkNGQ4NmZjOmRjOTI0YjRkLThjOGMtNDlkYy05MmI4LTAyNWNlZDRjZTBmNA==', verify_ssl_certs = False)
+
+class Chat:
+    def __init__(self):
+        self.messages = []
+
+    def start_chat(self, prompt):
+        self.messages = [SystemMessage(content = prompt)]
+
+    def add_message(self, user_input):
+        self.messages.append(HumanMessage(content=user_input))
+
+    def get_answer(self):
+        res = chat(self.messages)
+        self.messages.append(res)
+        return res.content
